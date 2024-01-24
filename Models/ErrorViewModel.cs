809bb958e51c9372a1bdde77cc976c394bc97306
Ezee1234 <akriti09bhardwaj@gmail.com>
2024-01-24 16:15:12 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspCore6_IdentityApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
